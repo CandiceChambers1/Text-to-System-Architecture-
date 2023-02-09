e8981062435a9b2cb390f332e712a9257cddb96c
Parth Ganeriwala <ganeriwalaparth@gmail.com>
2023-02-09 15:23:28 -0500

    Added Generation of XML Header and Footer

--- Java/src/main/Output.java
+++ Java/src/main/Output.java
@@ -5,6 +5,23 @@ public class Output {
     public Output(Sentences sentences){this.sentences = sentences;}
 
     public String generateOutput(){
-        return null;
+        String output = "<XMI xmi.version=\"1.1\" xmlns:UML=\"omg.org/UML1.3\">\n";
+        output += generateHeader();
+        output += generateFooter();
+        return output;
+    }
+    public String generateHeader(){
+        String output = "\t<XMI.header>\n" +
+                "\t\t<XMI.documentation>\n" +
+                "\t\t\t<XMI.exporter>Enterprise Architect</XMI.exporter>\n" +
+                "\t\t\t<XMI.exporterVersion>2.5</XMI.exporterVersion>\n" +
+                "\t\t</XMI.documentation>\n" +
+                "\t</XMI.header>";
+        return output;
+    }
+    public String generateFooter(){
+        String output = "\t<XMI.extensions xmi.extender=\"Enterprise Architect 2.5\"/>\n" +
+                "</XMI>";
+        return output;
     }
 }
