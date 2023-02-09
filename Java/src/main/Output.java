d7cf2fdab07c2380e9a79e32ec49f820744a6d57
Parth Ganeriwala <ganeriwalaparth@gmail.com>
2023-02-09 15:09:15 -0500

    Added OutputFormatter Class which will generate XML Files

---
+++ Java/src/main/Output.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Output {
+    Sentences sentences;
+    public Output(Sentences sentences){this.sentences = sentences;}
+
+    public String generateOutput(){
+        return null;
+    }
+}
