0d8c903b07fb72b0c3e26d16688d167141de1a39
candi <chambersc2017@my.fit.edu>
2025-02-25 12:32:22 -0500

    final edits

--- Java/src/main/CreateXmlFileDemo.java
+++ Java/src/main/CreateXmlFileDemo.java
@@ -6,7 +6,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
-//import javax.xml.transform.TransformerFactory;
+//import javax.gen.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Attr;
@@ -294,13 +294,13 @@ public class CreateXmlFileDemo {
         generateDiagram(doc,content,generateXMI_ID("other"),xmiPackageID);
         generateFooter(doc,rootElement);
 
-        // write the content into xml file
+        // write the content into gen.xml file
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         DOMSource source = new DOMSource(doc);
         String name= filename;
-        StreamResult result = new StreamResult(new File("src/xml/" + name +".xml"));
+        StreamResult result = new StreamResult(new File("src/gen.xml/" + name +".gen.xml"));
         transformer.transform(source, result);
 
 //         Output to console for testing
